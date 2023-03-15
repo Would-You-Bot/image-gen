fc369d29cf9d707016c86b8110af0b8459234f90
Dominik K <dominik@koch-bautechnik.de>
2023-03-15 21:11:05 +0100

    feat: text rendering

--- imageGen.cpp
+++ imageGen.cpp
@@ -2,21 +2,29 @@
 #define cimg_debug 1
 #endif
 #include "cimg/CImg.h"
+#include <iostream>
 using namespace cimg_library;
 #undef min
 #undef max
 
 int main() {
   CImg<unsigned char> image("template.bmp");
-  CImgDisplay display(image.width(), image.height(), "My Image");
+  const unsigned char* data = image.data();
+  CImgDisplay display(image.width(), image.height(), "Would You Image");
   display.display(image);
 
-  unsigned char white[] = { 255, 255, 255 };
-  image.draw_text(100, 100, "Hello, World!", white, 0, 24);
+  unsigned char black[] = { 0, 0, 0 };
+  image.draw_text(140, 141, "first text", black, 0, 1, 25, 0.5f, 0.5f);
+
+  image.draw_text(140, 220, "second text", black, 0, 1, 25, 0.5f, 0.5f);
   display.display(image);
+
+  image.save("output.bmp");
+
+  std::cout << "Image buffer: " << (void*)image.data() << std::endl;
 
   display.wait();
-  image.save("output.bmp");
+
 
   return 0;
 }
